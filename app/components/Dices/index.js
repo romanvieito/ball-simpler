5ded17889c3eec361f89f74f928335d94ab51161
Alberto Roman <romanvieito@gmail.com>
2017-10-23 15:19:56 -0400

    showing dice

--- app/components/Dices/index.js
+++ app/components/Dices/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 // import { FormattedMessage } from 'react-intl';
+import NormalImg from 'components/Img';
 
 // import A from 'components/A';
 // import LocaleToggle from 'containers/LocaleToggle';
 import Wrapper from './Wrapper';
 import './dices.css';
+import DiceImg1 from '../../images/dices/1c.gif';
+// import DiceImg2 from '../../images/dices/2c.gif';
+// import DiceImg3 from '../../images/dices/3c.gif';
+// import DiceImg4 from '../../images/dices/4c.gif';
+// import DiceImg5 from '../../images/dices/5c.gif';
+// import DiceImg6 from '../../images/dices/6c.gif';
+
 // import messages from './messages';
 
 function Dices() {
   return (
     <Wrapper>
-      <section>bb
+      <section>
+        <NormalImg src={DiceImg1} alt="1" />
       </section>
     </Wrapper>
   );
