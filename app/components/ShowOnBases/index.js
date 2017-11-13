c21c6df89d14e07fcd0757e0f2a2ac8f6a07a35b
Alberto Roman <romanvieito@gmail.com>
2017-11-13 17:02:23 -0500

    adding table in ShowOnBases

--- app/components/ShowOnBases/index.js
+++ app/components/ShowOnBases/index.js
@@ -7,13 +7,33 @@
 import React from 'react';
 // import styled from 'styled-components';
 
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import './component.css';
+
+// import { FormattedMessage } from 'react-intl';
+// import messages from './messages';
 
 function ShowOnBases() {
   return (
-    <div>
-      <FormattedMessage {...messages.header} />
+    <div className="comp-back-color">
+      {/* <FormattedMessage {...messages.header} /> */}
+      <table className="table table-sm">
+        <tbody>
+          <tr>
+            <td colSpan="2">McCarthy</td>
+          </tr>
+          <tr>
+            <td>HOU</td>
+            <td rowSpan="2"></td>
+          </tr>
+          <tr>
+            <td>LAD</td>
+          </tr>
+          <tr>
+            <td>^ 11</td>
+            <td className="text-right">0 Out</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
